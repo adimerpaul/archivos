e2b55ff4f9eaa22625146f3ca0f4af680cb71752
Alejandro <lopez.gutierrez.a07@gmail.com>
2021-06-15 10:12:55 -0400

    registro 4

--- js/registro.js
+++ js/registro.js
@@ -100,9 +100,9 @@ camposFormRegDocVacios = function(){
 			}
 		}
 	);
-	if($("#ini").val()!="0" || isNaN($("#ini").val()))
+	if( isNaN($("#ini").val()))
 		error.texto += "- Ini\n";
-	if($("#fin").val()!="0" || isNaN($("#fin").val()))
+	if( isNaN($("#fin").val()))
 		error.texto += "- Fin\n";
 	console.log($("#obs").val());
 	if(($("#obs").val()) == "" || $("#obs").val() == null) 
