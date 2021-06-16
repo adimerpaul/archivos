29ab922ade969b721141ea13d3528bf5964bc6a2
Alejandro <lopez.gutierrez.a07@gmail.com>
2021-06-16 15:00:55 -0400

    sd

--- phps/flistado.php
+++ phps/flistado.php
@@ -76,13 +76,13 @@ if(isset($_POST["opcion"])){
 				echo '0';
 			}
 			break; 
-			case "2":
+		case "2":
 				$docmen = new Documento();
 				$id=$_POST['iddoc'];
 				$datosdoc = $docmen->verDoc($id);
 				if ($datosdoc == 0) echo '0';
 				else
-				echo json_encode($datosdoc[0]);
+				echo ($datosdoc[0]);
 				break;	
 		case "3":
 				//echo "hola munod este es ".$_POST["numero"];
