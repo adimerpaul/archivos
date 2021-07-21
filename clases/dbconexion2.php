eb7ed399407a3995aad0cf881923889432fe2b55
adimerpaul <adimer101@gmail.com>
2021-07-21 13:18:20 -0400

    modifcacion para base de datos env

--- clases/dbconexion2.php
+++ clases/dbconexion2.php
@@ -5,19 +5,19 @@ class dbcnx{
 	var $password;
 	var $db;
 	var $cnx;
-	
-	function dbcnx($host = "localhost", $usuario = "root", $password = ""){
+
+	function dbcnx($host = "example_database", $usuario = "example_user", $password = "password"){
 		$this->host = $host;
 		$this->usuario = $usuario;
 		$this->password = $password;
-		
-		$this->cnx = @mysql_connect($this->host, $this->usuario, $this->password) or die("Error: No hay conexion."); 
+
+		$this->cnx = @mysql_connect($this->host, $this->usuario, $this->password) or die("Error: No hay conexion.");
 		//echo "acac";
 	}
-	
+
 	function db($db = "archivos"){
 		$this->db = $db;
 		@mysql_select_db($this->db, $this->cnx) or die("Error: Base de Datos Inexistente.");
-	}	
+	}
 }
 ?>
